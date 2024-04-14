f04d93d5d441a817aef38ae7ade2c3a3013e0dde
iAmBipinPaul <iambipinpaul@outlook.com>
2024-04-14 11:26:50 +0545

    Initial commit

---
+++ QuestPDF.Devanagari.Issue/Controllers/WeatherForecastController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace QuestPDF.Devanagari.Issue.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        QuestPDF.Settings.CheckIfAllTextGlyphsAreAvailable = false;
+        QuestPDF.Settings.License = LicenseType.Community;
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+    }
+    [HttpGet("CreatePDF")]
+    public  IActionResult CreatePDF()
+    {
+       
+      var res=  Document.Create(container =>
+        {
+            container
+                .Page(page =>
+                {
+                    page.Size(PageSizes.A4); ;
+                    page.Content()
+                        .Column(x =>
+                        {
+                            x.Item().Text("Mixed line: This 中文");
+                            x.Item().Text("감사합니다 korean");
+                            x.Item().Text("谢谢 chinese");
+                            x.Item().Text("ありがとう ございます japanese");
+                            x.Item().Text("நன்றி tamil");
+                            x.Item().Text("धन्यवाद Hindi");
+                            x.Item().Text("ขอบคุณ Thai");
+                            x.Item().Text("teşekkürler Turkish");
+                            x.Item().Text("ಧನ್ಯವಾದಗಳು Kannada");
+                        });
+                });
+        }).GeneratePdf();
+        return File(res, "application/pdf");
+    }
+}
